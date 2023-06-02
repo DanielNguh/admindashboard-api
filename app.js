36a2cafe74d92f7c359d64eacd20d6f95492df8f
danielnguh <nguhdaniel@gmail.com>
2023-06-02 02:11:45 +0100

    Initialized mongoose

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+const mongoose = require('morgan');
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+  console.log('Connected correctly to server');
+}, (err) => {console.log(err);})
 
 var app = express();
 
